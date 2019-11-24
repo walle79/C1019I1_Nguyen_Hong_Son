7cdd5f54bb1e1c458af166425c2d389994bbe470
walle79 <sablade7@gmail.com>
2019-11-24 23:25:16 +0700

    add file

---
+++ TaoFormLogin/script.js
@@ -0,0 +1,14 @@
+function login() {
+    var ten = document.getElementById("username").value;
+    regexp = /^[a-zA-Z0-9]{3,30}$/;
+    if(regexp.test(ten)){
+        console.log("Tên hợp lệ");
+    } else {
+        var loidangnhap = "Tên không hợp lệ! Phải từ 3-30 ký tự và không chứa ký tự đặc biệt!";
+        document.getElementById("login-false").innerHTML = loidangnhap;
+    }
+    var mk = document.getElementById("password").value;
+    if(ten == "0778767185" && mk == "sondanggett"){
+        window.location = "https://www.facebook.com/thuydang1007";
+    }
+}
